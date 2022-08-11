ea67b6b73d774819fd3fa78e7517efb431e49853
NenyeOsakwe <110306936+NenyeOsakwe@users.noreply.github.com>
2022-08-11 05:36:10 +0100

    Update the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp)
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`
+  }
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ]
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`
+}
+
 function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
@@ -10,6 +25,8 @@ function displayTemperature(response) {
   humidityElement.innerHTML = (response.data.main.humidity);
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "05c9a3df26c125e8d8cb65c733b4e8b2"
