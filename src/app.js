afd3a7de44893f42ee4af0b8e16b71cfa3da433d
NenyeOsakwe <110306936+NenyeOsakwe@users.noreply.github.com>
2022-08-11 05:40:08 +0100

    Removed console log

--- src/app.js
+++ src/app.js
@@ -16,7 +16,6 @@ function formatDate(timestamp) {
 function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   let descriptionElement = document.querySelector("#description");
