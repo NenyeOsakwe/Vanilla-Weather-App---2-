925ac4d4cc63dbfb2a0cb64140572c7c58382e42
NenyeOsakwe <110306936+NenyeOsakwe@users.noreply.github.com>
2022-08-21 16:48:32 +0100

    Implemented the unit conversion

--- src/app.js
+++ src/app.js
@@ -8,25 +8,36 @@ function formatDate(timestamp) {
   if (minutes < 10) {
     minutes = `0${minutes}`
   }
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ]
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ]
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`
 }
 
 function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.name;
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
   let descriptionElement = document.querySelector("#description");
-  descriptionElement.innerHTML = response.data.weather[0].description;
   let humidityElement = document.querySelector("#humidity");
-  humidityElement.innerHTML = (response.data.main.humidity);
   let windElement = document.querySelector("#wind");
-  windElement.innerHTML = Math.round(response.data.wind.speed);
   let dateElement = document.querySelector("#date");
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   let iconElement = document.querySelector("#icon");
+
+  celsiusTemperature = response.data.main.temp
+
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = (response.data.main.humidity);
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 
@@ -42,7 +53,34 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-search("New York")
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+
+  celsiusLink.classList.remove("active")
+  fahrenheitLink.classList.add("active")
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 3 + 12;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature)
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+
+  celsiusLink.classList.add("active")
+  fahrenheitLink.classList.remove("active")
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature)
+}
+
+let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form")
 form.addEventListener("submit", handleSubmit)
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link")
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature)
+
+let celsiusLink = document.querySelector("#celsius-link")
+celsiusLink.addEventListener("click", displayCelsiusTemperature)
+
+search("New York")
